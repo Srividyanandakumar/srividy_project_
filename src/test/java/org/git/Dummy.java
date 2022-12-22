ee0a2e4f49b23dfb10aa2271b89e8faba8767a2e
SrividyaNandakumar <srividyanandakumar@gmail.com>
2022-12-22 17:07:01 +0530

    Branch code

--- src/test/java/org/git/Dummy.java
+++ src/test/java/org/git/Dummy.java
@@ -5,6 +5,8 @@ public class Dummy {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world");
+		System.out.println("Happy Thursday");
+		System.out.println("december 22");
 	}
 
 }
